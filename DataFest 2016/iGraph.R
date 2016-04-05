c48229ec3830e884360c581363693d450c4d907f
Sarah Normoyle <snormoyle@wl-10-190-211-154.wireless.duke.edu>
2016-04-05 12:21:11 -0400

    final from iGraph code

--- DataFest 2016/iGraph.R
+++ DataFest 2016/iGraph.R
@@ -4,87 +4,140 @@
 library(data.table)
 library(dplyr)
 library(igraph)
+purchase = fread("approved_data_purchase-v5.csv")
 
-sampled = purchase_data
-#sampled = sample_n(purchase, 100000)
-#sampled = filter(sampled, la_event_type_cat == "CONCERTS")
-sampled = filter(sampled, venue_city == "NEW YORK")
-
+sampled = purchase
+sampled = filter(sampled, la_event_type_cat == "CONCERTS")
+sampled = filter(sampled, venue_city == "ATLANTA")
+names = sort(unique(sampled$primary_act_name))
 
 
 # make id's into integers
 sampled$purch_party_lkup_id = as.numeric(factor(sampled$purch_party_lkup_id))
-
-names = sort(unique(sampled$primary_act_name))
 sampled$primary_act_name = as.numeric(factor(sampled$primary_act_name))
 
 
-
-# store if more than one ticket was bought
+# store all tickets bought per person
 act_list = list()
 for (i in 1:max(sampled$purch_party_lkup_id )){
-  act = sampled$primary_act_name[which(sampled$purch_party_lkup_id  == i)]
-  act_list[[i]] = act
+	act = sampled$primary_act_name[which(sampled$purch_party_lkup_id  == i)]
+	act_list[[i]] = act
 }
 
 
 # function to get edges of markov pairs
 markov_pairs = function(thing){
-  n = length(thing)
-  pairs = matrix(nrow = n-1, ncol = 2)
-  for (i in 1:(n-1)){
-    pairs[i,] = c(thing[i], thing[i+1])
-  }
-  
-  nopairs = pairs[-(which(pairs[,1] == pairs[,2])),]
-  return(c(t(nopairs)))
+	n = length(thing)
+	pairs = matrix(nrow = n-1, ncol = 2)
+	for (i in 1:(n-1)){
+		pairs[i,] = c(thing[i], thing[i+1])
+	}
+	
+	matched = which(pairs[,1] == pairs[,2])
+
+	if (length(matched) > 0){
+		pairs = pairs[-matched,]
+	}
+	return(c(t(pairs)))
 }
 
 # get list of all makov pairs
+# and if bought more than one ticket
 all_pairs = c()
+#all_connect_tick = c()
 for (i in 1:length(act_list)){
-  if (length(act_list[[i]]) > 1){
-    edges = act_list[[i]]
-    all_pairs = c(all_pairs, markov_pairs(edges))
-    if (i%%1000 == 0){print(i)}
-  }
+	if (length(act_list[[i]]) > 1){
+		edge = act_list[[i]]
+		#all_connect_tick = c(all_connect_tick, edge)
+		all_pairs = c(all_pairs, markov_pairs(edge))
+	}
 }
 
+weights = (table(all_pairs))
+weights = data.frame(weights)$Freq
 
-vertex = unique(sampled$primary_act_name)
+
+
+# labels of connections
 with_connections = unique(all_pairs)
-
 label = sort(with_connections)
-all_pairs_new = all_pairs
 
+# re-label the pairs with integers
+all_pairs_new = all_pairs
 for (i in 1:length(all_pairs_new)){
-  all_pairs_new[i] = which(label == all_pairs[i])
+	all_pairs_new[i] = which(label == all_pairs[i])
 }
 
+#weight_label_new = weight_label
+#for (i in 1:length(weight_label)){
+#	weight_label_new[i] = which(label == weight_label[i])
+#}
+
+# re-label the pairs for order of weights
+#all_pairs_new_new = all_pairs_new
+#for (i in 1:length(all_pairs_new)){
+#	all_pairs_new_new[i] = which(all_pairs_new_new[i] == weight_label_new)
+#}
+
+label_names = names[label]
 
 vertex = c(1:length(with_connections))
 
-names[which(names == "Beyonc\xe9")] = "Beyonce"
-names[which(names == "Man\xe1")] = "Man"
-names[which(names == "La Arrolladora Banda Lim\xf3n")] = "La Arrolandora Banda Limon"
+g = graph.empty(directed = FALSE)
+g = g + vertices(vertex, color = "cornflower blue", label.cex = 0.35, 
+	size = 3.5, label = c(1:length(vertex)), label.dist = 0,
+	label.color = "black", font = 2)
+g = g + edges(all_pairs_new, color = "medium grey")
 
-radian.rescale <- function(x, start=0, direction=1) {
-  c.rotate <- function(x) (x + start) %% (2 * pi) * direction
-  c.rotate(scales::rescale(x, c(0, 2 * pi), range(x)))
-}
+# add special names of atlanta on graph
+vertex.attributes(g)$label[70] = label_names[70]
+vertex.attributes(g)$label.dist[70] = 0.2
+vertex.attributes(g)$label.cex[70] = 0.75
 
-lab.locs = radian.rescale(1:length(with_connections), -1, 0)
-g = graph.empty(directed = F)
-g = g + vertices(vertex, color = "black", label.cex = 0.5, label.dist = 0.1, label.degree = -pi/2)
-g = g + edges(all_pairs_new, color = "blue")
-layout <- layout.reingold.tilford(g, circular=T)
+vertex.attributes(g)$label[8] = "Beyonce"
+vertex.attributes(g)$label.dist[8] = -0.25
+vertex.attributes(g)$label.cex[8] = 0.75
 
-plot(g, vertex.size = 2.5, layout = layout, vertex.label = names[label], vertex.label.color = 'red', 
-     edge.width = 0.5)
-
-
-#plot in order of most connections
+vertex.attributes(g)$label[13] = label_names[13]
+vertex.attributes(g)$label.dist[13] = 0.2
+vertex.attributes(g)$label.cex[13] = 0.75
 
 
+vertex.attributes(g)$label[109] = label_names[109]
+vertex.attributes(g)$label.dist[109] = -0.25
+vertex.attributes(g)$label.cex[109] = 0.7
 
+
+vertex.attributes(g)$label[39] = label_names[39]
+vertex.attributes(g)$label.dist[39] = 0.25
+vertex.attributes(g)$label.cex[39] = 0.7
+
+# make pdf of graph
+pdf("Graph.pdf", width = 8, height = 8)
+plot(g, 
+	layout = layout.reingold.tilford(g, circular = T))
+dev.off()
+
+
+
+# names[label]
+
+# GRAPH 2: circle type graph
+
+#vertex = c(1:length(with_connections))
+
+#g = graph.empty(directed = FALSE)
+#g = g + vertices(vertex, color = "light blue", label.cex = 0.3)
+#g = g + edges(all_pairs_new_new, color = "grey")
+
+
+#pdf("Graph2.pdf", width = 8, height = 8)
+#plot(g, vertex.size = 5, 
+#	#layout = layout.reingold.tilford(g, circular = T),
+#	layout = layout.circle,
+#    vertex.label = c(1:length(vertex)),
+#    vertex.label.color = "black", 
+#	edge.width = 0.5)
+#title("Network of Atlanta Concert Tickets", cex.main = 1.5)
+#dev.off()
 
